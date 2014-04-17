db934f1f206912473c75217bb9d09726a8c97e1d
alec resnick <aresnickety@gmail.com>
2014-04-17 15:31:40 -0400

    Adding a simple .equals functionality to Array

--- util.js
+++ util.js
@@ -1,3 +1,22 @@
 // Some basic functions for degree/radian conversion
 function toRadians(degrees) { return degrees * (Math.PI / 180);}
 function toDegrees(radians) { return radians * (180 / Math.PI);}
+
+// Comparing arrays; unfortunately, JS does not have an ==; here we modify
+// the Array prototype directly
+Array.prototype.equals = function (array) {
+    if (!array) { return false; }
+    else if (this.length != array.length) { return false; }
+    // else
+    for (var i = 0, l = this.length; i < l; i++) {
+        // Check for nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse
+            if (!this[i].equals(array[i])) { return false; }
+        }
+        else if (this[i] != array[i]) { return false; }
+    }
+
+    // If we get to the end, we've checked all the elements
+    return true;
+};
