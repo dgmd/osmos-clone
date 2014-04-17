1f984ebbe7d2885518fc82f92f11899c5ef9a0ba
alec resnick <aresnickety@gmail.com>
2014-04-17 15:29:13 -0400

    A place for basic utilities, beginning with degree/radian conversion

---
+++ util.js
@@ -0,0 +1,3 @@
+// Some basic functions for degree/radian conversion
+function toRadians(degrees) { return degrees * (Math.PI / 180);}
+function toDegrees(radians) { return radians * (180 / Math.PI);}
